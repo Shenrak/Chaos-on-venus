2aa5631aaa9d638cef514d773de74e083ce951a3
Giom <giomgots@gmail.com>
2019-06-17 15:55:39 +0200

    ff

--- src/colony-repository/interfaces/workers.js
+++ src/colony-repository/interfaces/workers.js
@@ -1,4 +1,24 @@
+const { handleLambdaEvent } = require("../utils/event-handlers")
 const { queryArray } = require("./interfaces-tools")
 const { workers } = require("../state").state
 
-module.exports.getWorkers = ({query}) => queryArray(workers)({query})
+const sicken = async ({ nbHurt }) => {
+  const workers = await this.getWorkers({type: ""})
+  workers.sort(() => Math.random() - 0.5)
+
+  let i = 0
+  for (const worker in workers) {
+    if (!worker.dead && i < nbHurt) {
+      if (worker.sick) {
+        updateWorker({ id: worker.id, changes: [{ dead: true }] })
+      } else {
+        updateWorker({ id: worker.id, changes: [{ sick: true }] })
+      }
+      i++
+    }
+  }
+}
+
+module.exports.sickenHuman = handleLambdaEvent(sicken)
+
+module.exports.getWorkers = ({ query }) => queryArray(workers)({ query })
